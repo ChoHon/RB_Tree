acee5711925d5c95afdb2d31749f57ed4a3950aa
Chohon <siaksiakx@gmail.com>
2022-10-25 01:22:58 +0000

    modify test case

--- src/driver.c
+++ src/driver.c
@@ -5,29 +5,39 @@
 int main(int argc, char *argv[]) {
     rbtree *t = new_rbtree();
 
-    rbtree_insert(t, 8);
-    rbtree_insert(t, 7);
-    rbtree_insert(t, 9);
-    rbtree_insert(t, 3);
-    rbtree_insert(t, 6);
-    rbtree_insert(t, 4);
+    // 10, 5, 8, 34, 67, 23, 156, 24, 2, 12, 24, 36, 990, 25
+
+    rbtree_insert(t, 10);
     rbtree_insert(t, 5);
+    rbtree_insert(t, 8);
+    rbtree_insert(t, 34);
+    rbtree_insert(t, 67);
+    rbtree_insert(t, 23);
+    rbtree_insert(t, 156);
+    rbtree_insert(t, 24);
+    rbtree_insert(t, 2);
     rbtree_insert(t, 12);
+    rbtree_insert(t, 24);
+    rbtree_insert(t, 36);
+    rbtree_insert(t, 990);
+    rbtree_insert(t, 25);
 
     rbtree_display(t, t->root);
 
     node_t *n;
     n = rbtree_find(t, 6);
-    printf("%d\n", n->key);
+    if (n != NULL) {
+        printf("%d\n", n->key);
 
-    rbtree *sub_t = new_rbtree();
-    sub_t->root = n->right;
-    sub_t->nil = t->nil;
+        rbtree *sub_t = new_rbtree();
+        sub_t->root = n->right;
+        sub_t->nil = t->nil;
 
-    printf("%d\n", rbtree_min(sub_t)->key);
-    printf("%d\n", rbtree_max(sub_t)->key);
+        printf("%d\n", rbtree_min(sub_t)->key);
+        printf("%d\n", rbtree_max(sub_t)->key);
 
-    rbtree_erase(t, n);
+        rbtree_erase(t, n);
+    }
 
     rbtree_display(t, t->root);
 
